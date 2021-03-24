e8a40d52d9b1d3089c2f8e8fe3ac4a86fbd284ee
Maxim <maxim@smspower.org>
2021-03-24 14:04:10 +0000

    Added GitHub Actions build

---
+++ build_config_default.cmd
@@ -0,0 +1,26 @@
+rem ###########################################################################
+rem Default Configuration file, DO NOT edit, if you want to provide your own
+rem paths, please copy this file into "build_config.cmd", which will be ignored
+rem by git
+rem ###########################################################################
+
+rem ###########################################################################
+rem # BMP2TILE 0.50+
+set CONFIG_BMP2TILE="%~dp0\tools\BMP2Tile.exe"
+
+rem ###########################################################################
+rem # WLA-DX 2020+
+set CONFIG_WLAZ80="%~dp0\tools\wla-z80.exe"
+set CONFIG_WLALINK="%~dp0\tools\wlalink.exe"
+
+rem ###########################################################################
+rem # PYTHON 3.x+
+set CONFIG_PYTHON="python.exe"
+
+rem ###########################################################################
+rem # FLIPS
+set CONFIG_FLIPS="%~dp0\tools\flips.exe"
+
+rem ###########################################################################
+rem # EMULATORS
+set CONFIG_MEKA="C:\sms\meka\mekaw.exe"
---
+++ src/make.cmd
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+call ..\build_config_default.cmd
+if exist ..\build_config.cmd ( call ..\build_config.cmd )
+
+%CONFIG_BMP2TILE% images\font.png -noremovedupes -savetiles images\font.bin || exit /b
+
+set PYTHONIOENCODING=UTF8
+%CONFIG_PYTHON% tools.py encode_script "script.yaml" "trees.asm" "script.asm" || exit /b
+
+%CONFIG_WLAZ80% -o "sfg1-en.gg.asm.o" "sfg1-en.gg.asm" || exit /b
+echo [objects]>linkfile
+echo "sfg1-en.gg.asm.o">>linkfile
+%CONFIG_WLALINK% -d -r -v -S -A linkfile "sfg1-en.gg" || exit /b
+
+%CONFIG_FLIPS% --create "Shining Force Gaiden (J) [!].gg" "sfg1-en.gg" ..\sfg1-en.gg.ips
+%CONFIG_FLIPS% --create "Shining Force Gaiden (J) [!].gg" "sfg1-en.gg" ..\sfg1-en.gg.bps
+
+if "%1" == "play" start %CONFIG_MEKA% "sfg1-en.gg"
